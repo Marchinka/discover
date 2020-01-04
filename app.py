9d2af4ae05966618900517020f987e0b72836369
Marcello <marcello.rossi@techedgegroup.com>
2020-01-04 12:48:39 +0100

    mongo writing

--- app.py
+++ app.py
@@ -17,4 +17,4 @@ def run_engine():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True)
--- scraping/scraping_engine.py
+++ scraping/scraping_engine.py
@@ -1,3 +1,16 @@
+import pymongo
+from pymongo import ReplaceOne
+from pymongo.errors import BulkWriteError
+
+mongo_client = pymongo.MongoClient(
+    "ds259738.mlab.com:59738",
+    username='discover_adm',
+    password='Magheggio.89',
+    authSource='heroku_rxd08jx3',
+    retryWrites=False)
+mongo_db = mongo_client["heroku_rxd08jx3"]
+events_collection = mongo_db["events"]
+
 class Show:
     def __init__(self, title, start_date, end_date, location, description="", link=""):
         self.title = title
@@ -7,14 +20,17 @@ class Show:
         self.description = description
         self.link = link
 
-    def print_show(self):
-        print({
+    def get_dict(self):
+        return {
             "title": self.title,
             "location": self.location,
             "start_date": self.start_date,
             "end_date": self.end_date,
             "description": self.description,
-            "link": self.link})
+            "link": self.link}
+
+    def print_show(self):
+        print(self.get_dict())
 
 
 class ScrapingEngine:
@@ -29,7 +45,17 @@ class ScrapingEngine:
             print("Found", len(shows), "shows")
             all_shows = all_shows + shows
 
-        # for show in all_shows:
-        #     show.print_show()
+        dtos = []
+        bulk_op = events_collection.initialize_ordered_bulk_op()
+
+        for show in all_shows:
+            dto = show.get_dict()
+            dtos.append(dto)
+            bulk_op.find({'title': dto["title"]}).upsert().update({'$set': dto})
+
+        try:
+            bulk_op.execute()
+        except BulkWriteError as bwe:
+            print(bwe.details)
 
 
